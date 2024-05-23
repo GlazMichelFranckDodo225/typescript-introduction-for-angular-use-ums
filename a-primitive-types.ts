ff1b5ddc3f065314cf11b33506e2a89b349a5642
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-23 06:26:18 +0200

    Primitive Types

---
+++ a-primitive-types.ts
@@ -0,0 +1,15 @@
+// Primitive Types
+
+// number
+// let age: number;
+// age = 15; // Ok
+// age = "15"; // Error ==> Type 'string' is not assignable to type 'number'
+let age: number = 15;
+
+// string
+let username: string;
+username = "JohnDoe";
+
+// boolean
+let isInstructor: boolean;
+isInstructor = true // or false
